f893efafbd1f20b88b92792fc7ed5142805bac6c
mornir <jeromepott@hotmail.com>
2024-02-05 08:42:39 +0100

    feat: try alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import vue from '@astrojs/vue'
 
+import alpinejs from '@astrojs/alpinejs'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), vue()],
+  integrations: [
+    tailwind(),
+    icon(),
+    vue(),
+    alpinejs({ entrypoint: '/src/alpine' }),
+  ],
 })
