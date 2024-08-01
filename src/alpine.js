95f4d6584a59b3fcbcea8eb1d92467b8d2db7286
mornir <jeromepott@hotmail.com>
2024-08-01 15:26:32 +0200

    clean up

--- src/alpine.js
+++ src/alpine.js
@@ -2,14 +2,4 @@ import masonry from 'alpinejs-masonry'
 
 export default (Alpine) => {
   Alpine.plugin(masonry)
-  /*  Alpine.data('search', () => ({
-      searchPattern: '',
-      showBox: false,
-      selectedIndex: null,
-      get searchResults() {
-        if (!this.searchPattern) return []
-        this.showBox = true
-        return fuse.search(this.searchPattern).slice(0, 5)
-      },
-    })) */
 }
